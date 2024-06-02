56ee4c5d19398034b9b4095776cb3c7f2137435f
VARUNDKKANCHAN <119938091+VARUNDKKANCHAN@users.noreply.github.com>
2024-06-02 13:13:38 +0530

    Add files via upload

--- login/src/index.js
+++ login/src/index.js
@@ -190,6 +190,8 @@ app.use((err, req, res, next) => {
 });
 
 app.use(express.static('src/public'));
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
